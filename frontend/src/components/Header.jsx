4acb40a965a87885ba30acb8d63094101ed9fe7d
PoChuanSu <peterpeter1010@live.com>
2025-01-23 17:40:48 +1100

    router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,34 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap"; // npm i react-router-bootstrap
 import logo from "../assets/logo.png";
+
+// npm i react-router-dom@6.8.2 downgrade to solve issue
 
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">
-                        <img src={logo} alt="su-pro-logo" />
-                        SuShop
-                    </Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>
+                            <img src={logo} alt="su-pro-logo" />
+                            SuShop
+                        </Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="/cart">
-                                <FaShoppingCart /> Cart
-                            </Nav.Link>
-                            <Nav.Link href="/login">
-                                <FaUser /> Sign In
-                            </Nav.Link>
+                            <LinkContainer to="/cart">
+                                <Nav.Link>
+                                    <FaShoppingCart /> Cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link>
+                                    <FaUser /> Sign In
+                                </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
