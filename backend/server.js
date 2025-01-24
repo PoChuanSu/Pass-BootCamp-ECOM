9de272519548a766b3093a0b33ca427f5ac2fc14
PoChuanSu <peterpeter1010@live.com>
2025-01-24 11:25:26 +1100

    environment variable

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import products from "./products.js";
-const port = 4000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
