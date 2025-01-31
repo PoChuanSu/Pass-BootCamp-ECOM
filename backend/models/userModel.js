27c3f81fcd5dabf0e522dde4199f92da53129bd7
PoChuanSu <peterpeter1010@live.com>
2025-01-31 11:42:14 +1100

    basic auth

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import brycpt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
     {
@@ -23,6 +24,10 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await brycpt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
