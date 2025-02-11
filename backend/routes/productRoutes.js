f911dedb87291a8c8398f661e9a999dfb29a86dc
PoChuanSu <peterpeter1010@live.com>
2025-02-11 12:21:30 +1100

    create product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 import {
     getProducts,
     getProductById,
+    createdProduct,
 } from "../controllers/productController.js";
+import { admin, protect } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createdProduct);
 router.route("/:id").get(getProductById);
 
 export default router;
