fbc93f06715e5de00fae24871ef6663a4b8d89ab
PoChuanSu <peterpeter1010@live.com>
2025-02-11 18:10:20 +1100

    product updat / uploat BE

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,10 +4,11 @@ import {
     getProducts,
     getProductById,
     createdProduct,
+    updateProduct,
 } from "../controllers/productController.js";
 import { admin, protect } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createdProduct);
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
 
 export default router;
