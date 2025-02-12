b8ecaf5ad047af12c90e74c97a9a239e8ed2da0e
PoChuanSu <peterpeter1010@live.com>
2025-02-12 17:05:43 +1100

    listUser FE & BE

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -28,6 +28,7 @@ import ProfileScreen from "./screens/ProfileScreen";
 import OrderListScreen from "./screens/admin/OrderListScreen";
 import ProductListScreen from "./screens/admin/ProductListScreen";
 import ProductEditScreen from "./screens/admin/productEditScreen";
+import UserListScreen from "./screens/admin/UserListScreen";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -55,6 +56,7 @@ const router = createBrowserRouter(
                     path="/admin/product/:id/edit"
                     element={<ProductEditScreen />}
                 />
+                <Route path="/admin/userlist" element={<UserListScreen />} />
             </Route>
         </Route>
     )
