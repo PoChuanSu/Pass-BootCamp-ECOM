e27191c976d57e37a59e1cde3957e2c6729f4e93
PoChuanSu <peterpeter1010@live.com>
2025-02-12 12:12:50 +1100

    deleteproduct

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,10 +5,15 @@ import {
     getProductById,
     createdProduct,
     updateProduct,
+    deleteProduct,
 } from "../controllers/productController.js";
 import { admin, protect } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createdProduct);
-router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
+router
+    .route("/:id")
+    .get(getProductById)
+    .put(protect, admin, updateProduct)
+    .delete(protect, admin, deleteProduct);
 
 export default router;
