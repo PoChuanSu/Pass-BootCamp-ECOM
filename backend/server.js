82732a9bc91282a85548c5f12e3eecef153ce033
PoChuanSu <peterpeter1010@live.com>
2026-02-08 18:05:22 +1100

    deploysetting

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
-    res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+    res.send({ clientId: process.env.PAYPAL_CLIENT_ID }),
 );
 
 const __dirname = path.resolve();
